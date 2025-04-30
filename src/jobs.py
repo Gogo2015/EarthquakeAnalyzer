73d66e8a4180434db08d6c9f57c08758ee69902f
Gaurav Mitra <mitragaurav10@gmail.com>
2025-04-30 00:14:55 -0500

    fix

--- src/jobs.py
+++ src/jobs.py
@@ -5,7 +5,7 @@ from hotqueue import HotQueue
 import os
 
 _redis_ip = os.environ.get("REDIS_IP", "redis-db")
-_redis_port= '6379'
+_redis_port= os.environ.get('REDIS_PORT', '6379')
 
 rd = redis.Redis(host=_redis_ip, port=_redis_port, db=0)
 q = HotQueue("queue", host=_redis_ip, port=_redis_port, db=1)
